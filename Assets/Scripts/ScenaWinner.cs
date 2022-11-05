6541b6de53a7770a24aa39db16aafbaf6df33466
AraMilagros <araapalenque@gmail.com>
2022-11-05 17:05:36 -0300

    Se agregaron escena de Victoria y Derrota. Ara Palenque

---
+++ Assets/Scripts/ScenaWinner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenaWinner : MonoBehaviour
+{
+    public string levelName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //PlayLevel();
+            Invoke("Ganaste", 0.5f);
+        }
+    }
+
+    public void Ganaste()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
